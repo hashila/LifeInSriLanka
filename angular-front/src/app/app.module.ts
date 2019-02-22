6abfce658688730d75144802d088e9848b38b9d2
hashila <hashila.abeywardana1@gmail.com>
2019-02-22 08:43:05 +0530

    p lock json

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { DocumentationpageComponent } from './components/documentationpage/documentationpage.component';
 import { PackagespageComponent } from './components/packagespage/packagespage.component';
 import { CharitypageComponent } from './components/charitypage/charitypage.component';
@@ -17,7 +17,6 @@ import { NewpostComponent } from './components/newpost/newpost.component';
 import { IndexPageEditComponent } from './components/index-page-edit/index-page-edit.component';
 import { EditCharityPageComponent } from './components/edit-charity-page/edit-charity-page.component';
 import { EditPackagesComponent } from './components/edit-packages/edit-packages.component';
-import { EditComponent } from './components/edit/edit.component';
 import { ShowCustomerDetailsComponent } from './components/show-customer-details/show-customer-details.component';
 import { EditContactDetailsComponent } from './components/edit-contact-details/edit-contact-details.component';
 
@@ -38,7 +37,6 @@ import { EditContactDetailsComponent } from './components/edit-contact-details/e
     IndexPageEditComponent,
     EditCharityPageComponent,
     EditPackagesComponent,
-    EditComponent,
     ShowCustomerDetailsComponent,
     EditContactDetailsComponent
   ],
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT ||3000;
 
 app.use(express.static(path.join(__dirname,"frontend")));
